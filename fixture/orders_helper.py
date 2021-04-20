199d4b5f290f0d39717f5a9a78b1816cc9f137a5
NataliaMalinina <nat19@yandex.ru>
2021-04-20 22:58:25 +0700

    модернизирован метод получения айтемов для корзины - вынесено в фикстуру генерация айтемов. в параметрах захардкожены айдишники товаров

--- fixture/orders_helper.py
+++ fixture/orders_helper.py
@@ -1,16 +1,24 @@
-
+from random import randint
+from model import parameters
 
 class OrdersHelper:
 
     def __init__(self, app):
         self.app = app                                                                                                   #self.app.wd - если нужен метод из другого класса, то вызываем его так
 
-    def cart(self, amt, head, itemId, amount, deferred):
-        payload = []
-        for i in range(amt):
-            n = {"itemId": itemId[i], "amount": amount[i], "deferred": deferred[i]}
-            payload.append(n)
-        body = {"items": payload}
+    def generate_payload(self, cnt):
+        dataset = []
+        for i in range(cnt):
+            dataset.append(
+                {'itemId': parameters.items.pop(randint(0, len(parameters.items) - 1)),
+                 'amount': randint(1, 99),
+                 'deferred': False
+                 })
+        print(dataset)
+        return dataset
+
+    def cart(self, head, dataset):
+        body = {"items": dataset}
         return self.app._s.put(self.app.host + '/Cart', json=body, headers=head)
 
     def order(self, email, needEmail, needCall, customName, mnogoRuCardId, head):
