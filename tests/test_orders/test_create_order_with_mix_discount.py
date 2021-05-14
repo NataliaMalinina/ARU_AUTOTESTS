4beb0af5011e94f95b93e8391e9e947141ffb43d
NataliaMalinina <nat19@yandex.ru>
2021-05-14 16:02:56 +0700

    Тест - добить витаминками до макс скидки (использ промкоод 3%, витаминки 70) Тест - добить промиком до макс скидки (исп промик 7%, витаминки 30)

--- tests/test_orders/test_create_order_with_mix_discount.py
+++ tests/test_orders/test_create_order_with_mix_discount.py
@@ -51,3 +51,144 @@ def test_use_max_of_vitamins_and_max_discount_promocode(app):
     assert loads(ordering.text)['order']["vitaminsUsed"] == 100  #тест пока не работает - так как 500 - когда поправят - запустить для отладки
 
 
+def test_use_max_of_vitamins_and_any_promocode(app):
+    choice_autodest_before_order = app.choice_autodest(id=choice(parameters.autodestid_for_order),
+                                                       head=app.token_autorization())
+    formatted_json_str = pprint.pformat(choice_autodest_before_order.text)
+    print(choice_autodest_before_order.request.body)
+    print(choice_autodest_before_order, formatted_json_str, sep='\n\n')
+    assert choice_autodest_before_order.status_code == 200
+
+    put_the_item_in_the_cart = app.order_fixture.cart(dataset=app.order_fixture.generate_payload(2),
+                                                      head=app.token_autorization())
+    formatted_json_str = pprint.pformat(put_the_item_in_the_cart.text)
+    print(put_the_item_in_the_cart.request.body)
+    print(put_the_item_in_the_cart, formatted_json_str, sep='\n\n')
+    while loads(put_the_item_in_the_cart.text)['totalSum'] < 500:
+        put_the_item_in_the_cart = app.order_fixture.cart \
+            (dataset=app.order_fixture.generate_payload(2), head=app.token_autorization())
+        if loads(put_the_item_in_the_cart.text)['totalSum'] >= 500:
+            break
+    assert "\"tradeName\"" in put_the_item_in_the_cart.text
+    assert put_the_item_in_the_cart.status_code == 200
+
+    use_vitamins = app.order_fixture.cart_use_vitamins(vitaminsCount=100,
+                                                       head=app.token_autorization())
+
+    assert loads(use_vitamins.text)['vitaminsInfo']["vitaminsUsed"] > 0
+
+    use_promocode = app.order_fixture.cart_use_promocode(promoCode='RIGA2', head=app.token_autorization())
+
+    formatted_json_str = pprint.pformat(use_promocode.text)
+    print(use_promocode.request.body)
+    print(use_promocode, formatted_json_str, sep='\n\n')
+    assert loads(use_promocode.text)['promoCodes'][0]['isUsed'] == True
+    assert use_promocode.status_code == 200
+
+    ordering = app.order_fixture.create_order(email=None, needEmail=False, needCall=False, mnogoRuCardId=None,
+                                              head=app.token_autorization())
+    formatted_json_str = pprint.pformat(ordering.text)
+    print(ordering.request.body)
+    print(ordering, formatted_json_str, sep='\n\n')
+    assert ordering.status_code == 200
+    assert "\"orderId\"" in ordering.text
+    assert "\"orderNum\"" in ordering.text
+    assert len(loads(ordering.text)['order']['promoCodes']) != 0
+    assert loads(ordering.text)['order']["vitaminsUsed"] == 70
+
+
+def test_use_vitamins_and_promocode_for_max_discount_with_help_of_promocode(app):
+    choice_autodest_before_order = app.choice_autodest(id=choice(parameters.autodestid_for_order),
+                                                       head=app.token_autorization())
+    formatted_json_str = pprint.pformat(choice_autodest_before_order.text)
+    print(choice_autodest_before_order.request.body)
+    print(choice_autodest_before_order, formatted_json_str, sep='\n\n')
+    assert choice_autodest_before_order.status_code == 200
+
+    put_the_item_in_the_cart = app.order_fixture.cart(dataset=app.order_fixture.generate_payload(2),
+                                                      head=app.token_autorization())
+    formatted_json_str = pprint.pformat(put_the_item_in_the_cart.text)
+    print(put_the_item_in_the_cart.request.body)
+    print(put_the_item_in_the_cart, formatted_json_str, sep='\n\n')
+    while loads(put_the_item_in_the_cart.text)['totalSum'] < 500:
+        put_the_item_in_the_cart = app.order_fixture.cart \
+            (dataset=app.order_fixture.generate_payload(2), head=app.token_autorization())
+        if loads(put_the_item_in_the_cart.text)['totalSum'] >= 500:
+            break
+    assert "\"tradeName\"" in put_the_item_in_the_cart.text
+    assert put_the_item_in_the_cart.status_code == 200
+
+    use_vitamins = app.order_fixture.cart_use_vitamins(vitaminsCount=80,
+                                                       head=app.token_autorization())
+
+    assert loads(use_vitamins.text)['vitaminsInfo']["vitaminsUsed"] > 0
+
+    use_promocode = app.order_fixture.cart_use_promocode(promoCode='RIGA2', head=app.token_autorization())
+
+    formatted_json_str = pprint.pformat(use_promocode.text)
+    print(use_promocode.request.body)
+    print(use_promocode, formatted_json_str, sep='\n\n')
+    assert loads(use_promocode.text)['promoCodes'][0]['isUsed'] == True
+    assert use_promocode.status_code == 200
+
+    ordering = app.order_fixture.create_order(email=None, needEmail=False, needCall=False, mnogoRuCardId=None,
+                                              head=app.token_autorization())
+    formatted_json_str = pprint.pformat(ordering.text)
+    print(ordering.request.body)
+    print(ordering, formatted_json_str, sep='\n\n')
+    assert ordering.status_code == 200
+    assert "\"orderId\"" in ordering.text
+    assert "\"orderNum\"" in ordering.text
+    assert len(loads(ordering.text)['order']['promoCodes']) != 0
+    assert loads(ordering.text)['order']["vitaminsUsed"] == 70
+
+
+def test_use_vitamins_and_promocode_for_max_discount_with_help_of_vitamins(app):
+    choice_autodest_before_order = app.choice_autodest(id=choice(parameters.autodestid_for_order),
+                                                       head=app.token_autorization())
+    formatted_json_str = pprint.pformat(choice_autodest_before_order.text)
+    print(choice_autodest_before_order.request.body)
+    print(choice_autodest_before_order, formatted_json_str, sep='\n\n')
+    assert choice_autodest_before_order.status_code == 200
+
+    put_the_item_in_the_cart = app.order_fixture.cart(dataset=app.order_fixture.generate_payload(2),
+                                                      head=app.token_autorization())
+    formatted_json_str = pprint.pformat(put_the_item_in_the_cart.text)
+    print(put_the_item_in_the_cart.request.body)
+    print(put_the_item_in_the_cart, formatted_json_str, sep='\n\n')
+    while loads(put_the_item_in_the_cart.text)['totalSum'] < 500:
+        put_the_item_in_the_cart = app.order_fixture.cart \
+            (dataset=app.order_fixture.generate_payload(2), head=app.token_autorization())
+        if loads(put_the_item_in_the_cart.text)['totalSum'] >= 500:
+            break
+    assert "\"tradeName\"" in put_the_item_in_the_cart.text
+    assert put_the_item_in_the_cart.status_code == 200
+
+    use_promocode = app.order_fixture.cart_use_promocode(promoCode='MAИ', head=app.token_autorization())
+
+    formatted_json_str = pprint.pformat(use_promocode.text)
+    print(use_promocode.request.body)
+    print(use_promocode, formatted_json_str, sep='\n\n')
+    assert loads(use_promocode.text)['promoCodes'][0]['isUsed'] == True
+    assert use_promocode.status_code == 200
+
+    use_vitamins = app.order_fixture.cart_use_vitamins(vitaminsCount=70,
+                                                       head=app.token_autorization())
+
+    assert loads(use_vitamins.text)['vitaminsInfo']["vitaminsUsed"] > 0
+
+    ordering = app.order_fixture.create_order(email=None, needEmail=False, needCall=False, mnogoRuCardId=None,
+                                              head=app.token_autorization())
+    formatted_json_str = pprint.pformat(ordering.text)
+    print(ordering.request.body)
+    print(ordering, formatted_json_str, sep='\n\n')
+    assert ordering.status_code == 200
+    assert "\"orderId\"" in ordering.text
+    assert "\"orderNum\"" in ordering.text
+    assert len(loads(ordering.text)['order']['promoCodes']) != 0
+    assert loads(ordering.text)['order']["vitaminsUsed"] == 30
+
+
+
+
+
--- tests/test_orders/test_create_order_with_promo.py
+++ tests/test_orders/test_create_order_with_promo.py
@@ -172,6 +172,39 @@ def test_order_with_promocode_onceonly_false_unlimited_false(app):
         assert len(loads(ordering.text)['order']['promoCodes']) != 0
 
 
+def test_max_discount_promocode(app):
+    put_the_item_in_the_cart = app.order_fixture.cart(dataset=app.order_fixture.generate_payload(2),
+                                                      head=app.token_autorization())
+    formatted_json_str = pprint.pformat(put_the_item_in_the_cart.text)
+    print(put_the_item_in_the_cart.request.body)
+    print(put_the_item_in_the_cart, formatted_json_str, sep='\n\n')
+    while loads(put_the_item_in_the_cart.text)['totalSum'] < 500:
+        put_the_item_in_the_cart = app.order_fixture.cart \
+            (dataset=app.order_fixture.generate_payload(2), head=app.token_autorization())
+        if loads(put_the_item_in_the_cart.text)['totalSum'] >= 500:
+            break
+    assert "\"tradeName\"" in put_the_item_in_the_cart.text
+    assert put_the_item_in_the_cart.status_code == 200
+
+    use_promocode = app.order_fixture.cart_use_promocode(promoCode="KUPON", head=app.token_autorization())
+
+    formatted_json_str = pprint.pformat(use_promocode.text)
+    print(use_promocode.request.body)
+    print(use_promocode, formatted_json_str, sep='\n\n')
+    assert loads(use_promocode.text)['promoCodes'][0]['isUsed'] == True
+    assert use_promocode.status_code == 200
+
+    ordering = app.order_fixture.create_order(email=None, needEmail=False, needCall=False, mnogoRuCardId=None,
+                                              head=app.token_autorization())
+    formatted_json_str = pprint.pformat(ordering.text)
+    print(ordering.request.body)
+    print(ordering, formatted_json_str, sep='\n\n')
+    assert ordering.status_code == 200
+    assert "\"orderId\"" in ordering.text
+    assert "\"orderNum\"" in ordering.text
+    assert len(loads(ordering.text)['order']['promoCodes']) != 0
+
+
 def test_delete_promocode_from_the_cart(app):
     put_the_item_in_the_cart = app.order_fixture.cart(dataset=app.order_fixture.generate_payload(2),
                                                       head=app.token_autorization())
